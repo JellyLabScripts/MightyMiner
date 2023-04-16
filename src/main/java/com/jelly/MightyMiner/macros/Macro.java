df025d3a946c57c7405b4ec3e0908adb63b0b22d
Niroxbtw <85623921+Niroxbtw@users.noreply.github.com>
2023-04-16 18:04:24 +0200

    Update Macro.java

--- src/main/java/com/jelly/MightyMiner/macros/Macro.java
+++ src/main/java/com/jelly/MightyMiner/macros/Macro.java
@@ -5,6 +5,8 @@ import com.jelly.MightyMiner.baritone.automine.logging.Logger;
 import com.jelly.MightyMiner.events.BlockChangeEvent;
 import com.jelly.MightyMiner.features.FuelFilling;
 import com.jelly.MightyMiner.handlers.MacroHandler;
+import com.jelly.MightyMiner.macros.macros.CommissionMacro;
+import com.jelly.MightyMiner.utils.LogUtils;
 import com.jelly.MightyMiner.utils.PlayerUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.Packet;
@@ -12,10 +14,12 @@ import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderWorldEvent;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraft.util.BlockPos;
 
 public abstract class Macro {
     protected Minecraft mc = Minecraft.getMinecraft();
     protected boolean enabled = false;
+    public static boolean brokeBlock = false;
 
     public void toggle() {
         enabled = !enabled;
@@ -51,7 +55,7 @@ public abstract class Macro {
 
     public void onBlockChange(BlockChangeEvent event){
         if (event.pos.equals((Object) mc.thePlayer.rayTrace(5, 1).getBlockPos()) && !event.old.getBlock().equals(event.update.getBlock())) {
-            CommissionMacro.brokeBlock = true;
+            brokeBlock = true;
         }
     }
 
@@ -78,4 +82,5 @@ public abstract class Macro {
             MacroHandler.pickaxeSkillReady = false;
         }
     }
+
 }

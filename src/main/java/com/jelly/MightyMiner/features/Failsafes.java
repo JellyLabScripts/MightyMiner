c3d12d2e31c7719b533c7de90eb9e6e53c4cd8d5
Niroxbtw <85623921+Niroxbtw@users.noreply.github.com>
2023-04-17 18:18:10 +0200

    One false flag fix

--- src/main/java/com/jelly/MightyMiner/features/Failsafes.java
+++ src/main/java/com/jelly/MightyMiner/features/Failsafes.java
@@ -102,6 +102,7 @@ public class Failsafes {
     @SubscribeEvent
     public void onPacket(ReceivePacketEvent event) {
         if (CommissionMacro.isWarping()) return;
+        if (Macro.brokeBlockUnderPlayer) return;
         if (!MightyMiner.config.stopMacrosOnRotationCheck) return;
         if (macros.stream().noneMatch(Macro::isEnabled)) return;
         if (mc.thePlayer == null || mc.theWorld == null) return;

9e815a78db3718a996798e8dc972c4e8bb1eed61
Niroxbtw <85623921+Niroxbtw@users.noreply.github.com>
2023-04-19 18:25:04 +0200

    Update MightyMiner.java

--- src/main/java/com/jelly/MightyMiner/MightyMiner.java
+++ src/main/java/com/jelly/MightyMiner/MightyMiner.java
@@ -98,7 +98,6 @@ public class MightyMiner {
         MinecraftForge.EVENT_BUS.register(new Failsafes());
         MinecraftForge.EVENT_BUS.register(new AOTVWaypointsCommands());
         MinecraftForge.EVENT_BUS.register(new PlayerESP());
-        MinecraftForge.EVENT_BUS.register(new DrawerUtils());
         KeybindHandler.initializeCustomKeybindings();
         MacroHandler.initializeMacro();
 

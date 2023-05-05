257966e0f4d30149e6930c569bcf54a3d8b6ed38
Niroxbtw <85623921+Niroxbtw@users.noreply.github.com>
2023-05-06 01:34:04 +0200

    Update AOTVMacro.java

--- src/main/java/com/jelly/MightyMiner/macros/macros/AOTVMacro.java
+++ src/main/java/com/jelly/MightyMiner/macros/macros/AOTVMacro.java
@@ -17,8 +17,11 @@ import com.jelly.MightyMiner.utils.BlockUtils.BlockUtils;
 import com.jelly.MightyMiner.utils.HypixelUtils.MineUtils;
 import com.jelly.MightyMiner.utils.Timer;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.inventory.GuiInventory;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.util.*;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -54,6 +57,29 @@ public class AOTVMacro extends Macro {
     private static final Timer waitAfterKillTimer = new Timer();
 
     private boolean reScan = true;
+
+    private enum MiningSpeed {
+        OPEN_SB_MENU,
+        CHECK_SB_MENU,
+        GET_LORE,
+        NONE
+    }
+
+    private enum UseItem {
+        OPEN_INVENTORY,
+        CHECK_INVENTORY,
+        USE_ITEM,
+        NONE
+    }
+
+    private MiningSpeed miningSpeed = MiningSpeed.NONE;
+
+    private UseItem useItem = UseItem.NONE;
+
+    private final Timer miningSpeedDelay = new Timer();
+
+    private int pickaxe = -1;
+
 
     @Override
     public void onEnable() {
@@ -116,6 +142,21 @@ public class AOTVMacro extends Macro {
         baritone = new AutoMineBaritone(getAutoMineConfig());
         currentState = State.MINING;
         waitAfterKillTimer.reset();
+
+        pickaxe = PlayerUtils.getItemInHotbar(false, "Pick", "Gauntlet", "Drill");
+        if (pickaxe == -1) {
+            LogUtils.debugLog("No Pickaxe");
+            MacroHandler.disableScript();
+            return;
+        }
+
+        if (MightyMiner.config.fastMine) {
+            miningSpeed = MiningSpeed.OPEN_SB_MENU;
+            useItem = UseItem.OPEN_INVENTORY;
+            miningSpeedDelay.reset();
+        } else {
+            miningSpeed = MiningSpeed.NONE;
+        }
     }
 
     @Override
@@ -165,7 +206,93 @@ public class AOTVMacro extends Macro {
         if (mc.thePlayer == null || mc.theWorld == null) return;
         if (MightyMiner.aotvWaypoints.getSelectedRoute() == null) return;
 
-        if (PlayerUtils.hasOpenContainer()) return;
+        switch (miningSpeed) {
+            case OPEN_SB_MENU:
+                switch (useItem) {
+                    case OPEN_INVENTORY:
+                        if (miningSpeedDelay.hasReached(100)) {
+                            mc.thePlayer.inventory.currentItem = pickaxe;
+
+                            InventoryUtils.openInventory();
+
+                            miningSpeedDelay.reset();
+                            useItem = UseItem.CHECK_INVENTORY;
+                        }
+                        break;
+                    case CHECK_INVENTORY:
+                        if (miningSpeedDelay.hasReached(500)) {
+                            if (mc.currentScreen instanceof GuiInventory) {
+                                useItem = UseItem.USE_ITEM;
+                            } else {
+                                useItem = UseItem.OPEN_INVENTORY;
+                            }
+
+                            miningSpeedDelay.reset();
+                        }
+                        break;
+                    case USE_ITEM:
+                        if (miningSpeedDelay.hasReached(300)) {
+                            InventoryUtils.clickOpenContainerSlot(44);
+
+                            miningSpeedDelay.reset();
+                            miningSpeed = MiningSpeed.CHECK_SB_MENU;
+                            useItem = UseItem.OPEN_INVENTORY;
+                        }
+                        break;
+                    case NONE:
+                        LogUtils.debugLog("Not in a use item state");
+                        MacroHandler.disableScript();
+                        break;
+                }
+                break;
+            case CHECK_SB_MENU:
+                if (miningSpeedDelay.hasReached(500)) {
+                    if (InventoryUtils.getInventoryName() != null && InventoryUtils.getInventoryName().contains("SkyBlock Menu")) {
+                        miningSpeedDelay.reset();
+                        miningSpeed = MiningSpeed.GET_LORE;
+                    } else {
+                        miningSpeedDelay.reset();
+                        useItem = UseItem.OPEN_INVENTORY;
+                        miningSpeed = MiningSpeed.OPEN_SB_MENU;
+                    }
+                }
+                break;
+            case GET_LORE:
+                if (miningSpeedDelay.hasReached(100)) {
+                    ItemStack itemStack = InventoryUtils.getStackInOpenContainerSlot(13);
+
+                    if (itemStack != null) {
+                        NBTTagList lore = InventoryUtils.getLore(itemStack);
+                        if (lore != null) {
+                            for (int i = 0; i < lore.tagCount(); i++) {
+                                if (lore.get(i).toString().contains("Mining Speed")) {
+                                    MacroHandler.miningSpeed = Integer.parseInt(lore.get(i).toString().substring(19).replaceAll("[\\D]", ""));
+
+                                    mc.thePlayer.closeScreen();
+
+                                    miningSpeed = MiningSpeed.NONE;
+                                    return;
+                                }
+                            }
+                        } else {
+                            LogUtils.debugLog("No item lore");
+                            MacroHandler.disableScript();
+                            return;
+                        }
+                    } else {
+                        LogUtils.debugLog("No item stack");
+                        MacroHandler.disableScript();
+                        return;
+                    }
+                }
+                break;
+            case NONE:
+        }
+
+        if (miningSpeed != MiningSpeed.NONE) return;
+
+
+
 
         ArrayList<AOTVWaypointsStructs.Waypoint> Waypoints = MightyMiner.aotvWaypoints.getSelectedRoute().waypoints;
 

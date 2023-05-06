610b6a23342a5f2a0541a40755460d187b1eb4b2
Niroxbtw <85623921+Niroxbtw@users.noreply.github.com>
2023-05-06 16:38:49 +0200

    Update PingAlert.java

--- src/main/java/com/jelly/MightyMiner/features/PingAlert.java
+++ src/main/java/com/jelly/MightyMiner/features/PingAlert.java
@@ -1,9 +1,14 @@
 package com.jelly.MightyMiner.features;
 
 import com.jelly.MightyMiner.utils.Clock;
+import com.jelly.MightyMiner.utils.LogUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class PingAlert {
     private static final Clock pingAlertClock = new Clock();
@@ -30,10 +35,44 @@ public class PingAlert {
 
         if (pingAlertClock.isScheduled() && pingAlertClock.passed()) {
             mc.theWorld.playSound(mc.thePlayer.posX, mc.thePlayer.posY, mc.thePlayer.posZ, "random.orb", 10.0F, 1.0F, false);
+            try {
+                SystemTray tray = SystemTray.getSystemTray();
+                createNotification("You got staff checked go in minecraft and verify the situation", tray, TrayIcon.MessageType.WARNING);
+            } catch (UnsupportedOperationException e) {
+                LogUtils.debugLog("Notifications are not supported on this system");
+            }
             pingAlertClock.schedule(100);
             numPings--;
         } else if (!pingAlertClock.isScheduled()) {
             pingAlertClock.schedule(100);
         }
+    }
+
+    public static void createNotification(String text, SystemTray tray, TrayIcon.MessageType messageType) {
+        new Thread(() -> {
+
+
+            if(Minecraft.isRunningOnMac) {
+                try {
+                    Runtime.getRuntime().exec(new String[]{"osascript", "-e", "display notification"});
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                TrayIcon trayIcon = new TrayIcon(new BufferedImage(1, 1, BufferedImage.TYPE_3BYTE_BGR), "Mighty Miner Failsafe Notification");
+                trayIcon.setToolTip("Mighty Miner Failsafe Notification");
+                try {
+                    tray.add(trayIcon);
+                } catch (AWTException e) {
+                    throw new RuntimeException(e);
+                }
+
+                trayIcon.displayMessage("Mighty Miner - Failsafes", text, messageType);
+
+                tray.remove(trayIcon);
+            }
+
+        }).start();
+
     }
 }

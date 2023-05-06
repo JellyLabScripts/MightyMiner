d3964e7abeb8d78f9828c3b4b074bc08a3eb6cb7
Niroxbtw <85623921+Niroxbtw@users.noreply.github.com>
2023-05-06 16:30:49 +0200

    Update MithrilMacro.java

--- src/main/java/com/jelly/MightyMiner/macros/macros/MithrilMacro.java
+++ src/main/java/com/jelly/MightyMiner/macros/macros/MithrilMacro.java
@@ -6,6 +6,7 @@ import com.jelly.MightyMiner.baritone.automine.config.BaritoneConfig;
 import com.jelly.MightyMiner.baritone.automine.config.MiningType;
 import com.jelly.MightyMiner.features.Failsafes;
 import com.jelly.MightyMiner.features.FuelFilling;
+import com.jelly.MightyMiner.features.PingAlert;
 import com.jelly.MightyMiner.handlers.KeybindHandler;
 import com.jelly.MightyMiner.handlers.MacroHandler;
 import com.jelly.MightyMiner.macros.Macro;
@@ -303,7 +304,7 @@ public class MithrilMacro extends Macro {
 
         if (staffCheckCounter > 2) {
             LogUtils.addMessage("Unbreakable block staff check or wrong stuck time threshold");
-            PlayerUtils.sendPingAlert();
+            PingAlert.sendPingAlert();
             staffCheckCounter = 0;
             inStaffCheck = true;
             return;

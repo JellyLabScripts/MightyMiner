e967a5382e4dec4489486957c0dc949185d3af48
Nima0908 <129191087+Nima0908@users.noreply.github.com>
2024-04-15 19:51:28 +0200

    Update PowderMacro.java

--- src/main/java/com/jelly/MightyMiner/macros/macros/PowderMacro.java
+++ src/main/java/com/jelly/MightyMiner/macros/macros/PowderMacro.java
@@ -39,7 +39,7 @@ public class PowderMacro extends Macro {
     private final Block[] obstacleBlocks = {Blocks.stained_glass, Blocks.stained_glass_pane, Blocks.wool, Blocks.prismarine, Blocks.chest};
 
     private final Block[] caveBlocks = {Blocks.bedrock, Blocks.flowing_water, Blocks.water, Blocks.flowing_lava, Blocks.lava,
-            Blocks.dirt,  Blocks.stained_hardened_clay, Blocks.hardened_clay, Blocks.wooden_slab, Blocks.log, Blocks.log2, // goblin holdout
+            Blocks.dirt, Blocks.stained_hardened_clay, Blocks.hardened_clay, Blocks.wooden_slab, Blocks.log, Blocks.log2, // goblin holdout
             Blocks.red_flower, Blocks.yellow_flower, Blocks.clay, Blocks.leaves, Blocks.leaves2, Blocks.grass, Blocks.tallgrass, // jungle
             Blocks.cobblestone, Blocks.stone_slab, Blocks.stone_slab2, Blocks.double_stone_slab2, Blocks.double_stone_slab, Blocks.wool, Blocks.stone_stairs, Blocks.stone_brick_stairs, Blocks.oak_fence, Blocks.cobblestone_wall, // precursor remnants
             Blocks.spruce_stairs, Blocks.acacia_stairs, Blocks.birch_stairs, Blocks.dark_oak_stairs, Blocks.jungle_stairs, Blocks.oak_stairs}; // mithril deposits
@@ -96,7 +96,7 @@ public class PowderMacro extends Macro {
 
                 if (closestLaneWithNoObstacles == 0) {
                     alignWithBlock();
-                } else if (closestLaneWithNoObstacles < 0) {
+                } else if (closestLaneWithNoObstacles < 0){
                     KeybindHandler.setKeyBindState(KeybindHandler.keybindA, true);
                     KeybindHandler.setKeyBindState(KeybindHandler.keybindD, false);
                 } else {
@@ -114,23 +114,12 @@ public class PowderMacro extends Macro {
             case TREASURE_WALK:
                 KeybindHandler.setKeyBindState(KeybindHandler.keybindW, true);
                 KeybindHandler.setKeyBindState(KeybindHandler.keybindAttack, !fitsPlayer(getRelativeBlockPos(0, -1, 1)));
-
-                float instantaneous_required_yaw;
-                float target_pitch;
-
-                EnumFacing facing = EnumFacing.fromAngle(mc.thePlayer.rotationYaw);
-                BlockPos nextTo = getPlayerLoc().offset(facing);
-
-                if (hasObstaclesToChest && mc.theWorld.getBlockState(nextTo).getBlock() != Blocks.air) {
-                    instantaneous_required_yaw = getRequiredYawCenter(nextTo);
-                    target_pitch = getRequiredPitchCenter(nextTo);
-                } else {
-                    instantaneous_required_yaw = getRequiredYawCenter(targetChest);
-                    target_pitch = hasObstaclesToChest ? 30 : getRequiredPitchCenter(targetChest);
-                }
+                float instantaneous_required_yaw = getRequiredYawCenter(targetChest);
+                float target_pitch = hasObstaclesToChest ? 30 : getRequiredPitchCenter(targetChest);
 
                 rotator.initAngleLock(instantaneous_required_yaw, target_pitch,
                         Math.max(getYawRotationTime(instantaneous_required_yaw, 45, 200, 500), getPitchRotationTime(target_pitch, 30, 200, 500)));
+
                 break;
 
             case TREASURE_SOLVE:
@@ -162,12 +151,13 @@ public class PowderMacro extends Macro {
                 targetChest = chestQueue.poll();
                 timeoutTimer.schedule(10000);
 
-                Vec3 feetCenter = getBlockCenter(getPlayerLoc());
-                Vec3 targetCenter = getBlockCenter(targetChest);
+                hasObstaclesToChest = false;
 
-                BlockPos trace = bresenham(feetCenter, new Vec3(targetCenter.xCoord, feetCenter.yCoord, targetCenter.zCoord));
-
-                hasObstaclesToChest = trace != null && !trace.equals(targetChest);
+                for (BlockPos pos : getAllBlocksInLine2d(getPlayerLoc(), targetChest)) {
+                    if (mc.theWorld.getBlockState(pos).getBlock() != Blocks.air) {
+                        hasObstaclesToChest = true;
+                    }
+                }
 
                 break;
             case TREASURE_WALK:
@@ -179,7 +169,7 @@ public class PowderMacro extends Macro {
                     return;
 
                 currentState = State.TREASURE_SOLVE;
-                rotator.easeTo(getRequiredYawCenter(targetChest), getRequiredPitchCenter(targetChest),
+               rotator.easeTo(getRequiredYawCenter(targetChest), getRequiredPitchCenter(targetChest),
                         Math.max(getYawRotationTime(getRequiredYawCenter(targetChest), 45, 150, 350),
                                 getPitchRotationTime(getRequiredPitchCenter(targetChest), 30, 150, 350)));
                 KeybindHandler.resetKeybindState();
@@ -254,10 +244,7 @@ public class PowderMacro extends Macro {
                 KeybindHandler.setKeyBindState(KeybindHandler.keybindD, playerZ > blockZ - 0.2);
                 break;
         }
-    }
-
-
-    @Override
+    }@Override
     public void onLastRender(RenderWorldLastEvent event) {
         if(rotator.rotating)
             rotator.update();
@@ -270,11 +257,9 @@ public class PowderMacro extends Macro {
     }
 
     @Override
-    public void onOverlayRenderEvent(RenderGameOverlayEvent event) {
-        if(event.type == RenderGameOverlayEvent.ElementType.TEXT){
-            mc.fontRendererObj.drawString(currentState + " " + currentState, 5 , 5, -1);
-            mc.fontRendererObj.drawString("Chests in waiting queue: " + chestQueue.size() + " | Chests in finished queue: " + solvedOrSolvingChests.size() + "/3", 5 , 17, -1);
-        }
+    public void onRenderGameOverlayEvent(RenderGameOverlayEvent.Text event) {
+        mc.fontRendererObj.drawString(currentState + " " + currentState, 5 , 5, -1);
+        mc.fontRendererObj.drawString("Chests in waiting queue: " + chestQueue.size() + " | Chests in finished queue: " + solvedOrSolvingChests.size() + "/3", 5 , 17, -1);
     }
 
     @Override
@@ -288,14 +273,7 @@ public class PowderMacro extends Macro {
             }, 200);
         } else if(message.contains("You have successfully picked the lock on this chest")) {
             currentState = State.NORMAL;
-        } else if(message.contains("You cannot mine this close to an entrance")) {
-            if(currentState != State.NORMAL)
-                return;
-
-            System.out.println(railwayOnLeftOrRight());
-
-            // TODO: SCAN RAILWAYS
-        } else if (message.contains("You must pick the lock on this chest to open it!")) {
+        } else if(message.contains("You must pick the lock on this chest to open it!")) {
             Logger.log("You don't have great explorer so no need for it to be enabled");
             MightyMiner.config.powGreatExplorer = false;
         }
@@ -334,7 +312,7 @@ public class PowderMacro extends Macro {
 
     private void addChestToQueue() {
         LogUtils.debugLog("Adding chest to queue");
-        List<BlockPos> foundBlocks = BlockUtils.findBlock(new Box(-7, 7, 3, -1, -7, 7),
+        List<BlockPos> foundBlocks = BlockUtils.findBlock(new Box(-7, 7, 3,-1, -7, 7),
                 new ArrayList<>(solvedOrSolvingChests), 0, 256, new BlockData<>(Blocks.chest));
 
         if(foundBlocks.isEmpty()){
@@ -348,70 +326,33 @@ public class PowderMacro extends Macro {
     }
 
 
-
-    private int countObstaclesInLane(int range, Block... obstaclesToCheck) {
-        int obstacles = 0;
-        for(int i = 0; i <= range; i++){
-            BlockPos checkPos = getRelativeBlockPos(0, 0, i, rotationYawAxis);
-            if(Arrays.asList(obstaclesToCheck).contains(getBlock(checkPos)))
-                ++obstacles;
-            if(Arrays.asList(obstaclesToCheck).contains(getBlock(checkPos.up())))
-                ++obstacles;
-
-        }
-        return obstacles;
+    public static double getRequiredYaw(double deltaX, double deltaZ) {
+        double angle = Math.atan2(deltaZ, deltaX) * 180.0 / Math.PI;
+        angle = normalizeAngle(angle);
+        return angle;
     }
 
-    // Returns -1 if the obstacle is on the left, 1 if the closest obstacle is on the right
-    private int closestObstaclesLeftOrRight(int halfScanRange, Block... obstaclesToCheck) {
-        for(int i = 0; i <= halfScanRange; i++){
-            if(Arrays.asList(obstaclesToCheck).contains(getBlock(getRelativeBlockPos(i, 0, 0, rotationYawAxis))))
-                return 1;
-            else if(Arrays.asList(obstaclesToCheck).contains(getBlock(getRelativeBlockPos(-i, 0, 0, rotationYawAxis))))
-                return -1;
-        }
-        return 1;
+    public static double getRequiredPitch(double deltaX, double deltaY, double deltaZ) {
+        double pitch = Math.atan2(Math.sqrt(deltaX * deltaX + deltaZ * deltaZ), deltaY) * 180.0 / Math.PI;
+        pitch = normalizeAngle(pitch);
+        return pitch;
     }
 
-    // Returns -1 if the railway is on the left, otherwise 1
-    private int railwayOnLeftOrRight() {
-        for(int i = 0; i <= 20; i++) {
-            for(int y = -20; y <= 20; y++) {
-                for(int j = 0; j <= 20; j++) {
-                    if(getRelativeBlock(i, y, j).equals(Blocks.rail))
-                        return 1;
-                }
-            }
-        }
-        return -1;
-
+    public static double normalizeAngle(double angle) {
+        while (angle > 180.0) angle -= 360.0;
+        while (angle < -180.0) angle += 360.0;
+        return angle;
     }
 
-
-    private int getClosestLaneWithNoObstacles(int range, Block... obstaclesToCheck) {
-        boolean leftHaveObstacles = false, rightHaveObstacles = false;
-        for(int i = 0; i <= 5; i++){
-            for(int j = 0; j <= range; j++){
-                BlockPos checkPosRight = getRelativeBlockPos(i, 0, j, rotationYawAxis);
-                BlockPos checkPosLeft = getRelativeBlockPos(-i, 0, j, rotationYawAxis);
-                if(Arrays.asList(obstaclesToCheck).contains(getBlock(checkPosRight)) || Arrays.asList(obstaclesToCheck).contains(getBlock(checkPosRight.up()))) {
-                    rightHaveObstacles = true;
-                }
-                if(Arrays.asList(obstaclesToCheck).contains(getBlock(checkPosLeft)) || Arrays.asList(obstaclesToCheck).contains(getBlock(checkPosLeft.up()))) {
-                    leftHaveObstacles = true;
-                }
-            }
-
-            if(!leftHaveObstacles)
-                return -i;
-            else if(!rightHaveObstacles)
-                return i;
-
-            leftHaveObstacles = false;
-            rightHaveObstacles = false;
-        }
-        LogUtils.debugLog("No lane with no obstacles found!");
-        return 0;
+    @Override
+    public void onDeactivated() {
+        if(rotator != null && rotator.rotating)
+            rotator.reset();
     }
 
+    public enum State {
+        TREASURE_SOLVE,
+        TREASURE_SOLVED,
+        NORMAL
+    }
 }

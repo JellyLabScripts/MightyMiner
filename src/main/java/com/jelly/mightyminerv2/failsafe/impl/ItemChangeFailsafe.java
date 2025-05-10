98213ab08442330d2cfe7e42550163352d8578d9
Jookly <jookly457@gmail.com>
2025-05-11 00:59:24 +1000

    changed log message

--- src/main/java/com/jelly/mightyminerv2/failsafe/impl/ItemChangeFailsafe.java
+++ src/main/java/com/jelly/mightyminerv2/failsafe/impl/ItemChangeFailsafe.java
@@ -128,7 +128,7 @@ public class ItemChangeFailsafe extends AbstractFailsafe {
                 log("Tracked necessary item '" + newDisplayName + "' (ID: " + newItemId + ") from original slot " + originalSlotOfThisItem + " now in slot " + slot + ".");
 
                 if (!originalSlotOfThisItem.equals(slot)) {
-                    warn("Necessary item '" + newDisplayName + "' (ID: " + newItemId + ") was MOVED from slot " + originalSlotOfThisItem + " to slot " + slot + ". Triggering failsafe!");
+                    log("Necessary item '" + newDisplayName + "' (ID: " + newItemId + ") was MOVED from slot " + originalSlotOfThisItem + " to slot " + slot + ". Triggering failsafe!");
                     return true;
                 }
             }

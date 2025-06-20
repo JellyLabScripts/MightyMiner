0bc99202878f9dc36effdbf21b05666955a50299
Jookly <jookly457@gmail.com>
2025-06-21 00:52:29 +1000

    stop failsafe triggering unintentionally

--- src/main/java/com/jelly/mightyminerv2/failsafe/impl/NameMentionFailsafe.java
+++ src/main/java/com/jelly/mightyminerv2/failsafe/impl/NameMentionFailsafe.java
@@ -3,13 +3,17 @@ package com.jelly.mightyminerv2.failsafe.impl;
 import com.jelly.mightyminerv2.failsafe.AbstractFailsafe;
 import com.jelly.mightyminerv2.macro.MacroManager;
 import lombok.Getter;
-import net.minecraft.client.Minecraft;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class NameMentionFailsafe extends AbstractFailsafe {
 
     @Getter
     private static final NameMentionFailsafe instance = new NameMentionFailsafe();
+
+    private static final Pattern SENDER_NAME_PATTERN = Pattern.compile("^.*?(?<senderName>[a-zA-Z0-9_]+)§?f?:");
 
     @Override
     public int getPriority() { return 10; }
@@ -23,13 +27,20 @@ public class NameMentionFailsafe extends AbstractFailsafe {
     @Override
     public boolean onChat(ClientChatReceivedEvent event) {
         String playerName = mc.thePlayer.getName();
-        String message = event.message.getUnformattedText();
-        if (message.contains(playerName)) {
-            warn("Your name was mentioned in chat:");
-            return true;
-        }
+        String unformattedMessage = event.message.getUnformattedText();
 
-        return false;
+        String senderName = null;
+        Matcher matcher = SENDER_NAME_PATTERN.matcher(unformattedMessage);
+        if (matcher.find()) { senderName = matcher.group("senderName"); }
+
+        if (senderName != null && senderName.equalsIgnoreCase(playerName)) { return false; }
+
+        if (unformattedMessage.toLowerCase().contains(playerName.toLowerCase() + " invited ")) { return false; }
+
+        //if (unformattedMessage.toLowerCase().contains("has invited you to join their party!")) { notify() }
+
+        Pattern mentionPattern = Pattern.compile("\\b" + Pattern.quote(playerName) + "\\b", Pattern.CASE_INSENSITIVE);
+        return mentionPattern.matcher(unformattedMessage).find();
     }
 
     @Override
